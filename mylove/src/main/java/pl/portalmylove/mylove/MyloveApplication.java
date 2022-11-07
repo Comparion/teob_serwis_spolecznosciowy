51919ae2e30682ba70c777225c20c3db486d1ea1
Patryk Banaś <banan725@gmail.com>
2022-11-07 17:47:46 +0100

    portal myLove

---
+++ mylove/src/main/java/pl/portalmylove/mylove/MyloveApplication.java
@@ -0,0 +1,26 @@
+package pl.portalmylove.mylove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**Klasa glowna uruchamiajaca aplikacje */
+@SpringBootApplication
+public class MyloveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyloveApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+						.allowedHeaders("*");
+			}
+		};
+	}
+}
